06eb276247fde9205e07cb05b62ce23c166e8397
Ivan Fernandez Mendez <ivanfernandezmen@gmail.com>
2026-04-28 20:48:56 +0200

    Páginas de error

--- src/pages/error/Forbidden.tsx
+++ src/pages/error/Forbidden.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { Main } from "../../components/Main/Main";
-import { TitlePage } from "../../components/TitlePage/TitlePage";
 import { Button } from "../../components/Button/Button";
 
 export function Forbidden() {
@@ -13,9 +12,12 @@ export function Forbidden() {
 
   return (
     <Main>
-      <TitlePage>Página no permitida</TitlePage>
-      <p className="text-lg sm:text-2xl text-center">No tienes lo permisos suficientes para acceder a esta página.</p>
-      <Button id="btnBackHome" type="button" handleClick={clickBackHomeHandler} width="fit" variant="secondary">Volver a la página de inicio</Button>
+      <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/4 xl:w-2/4 flex flex-col gap-3 sm:items-start items-center p-3">
+        <span className="text-9xl font-extrabold">403</span>
+        <h3 className="text-3xl">Página no permitida</h3>
+        <p className="text-lg sm:text-2xl w-full text-center sm:text-start">Lo sentimos, no tienes permisos para ver esta página. Si necesitas acceso, ponte en contacto con soporte.</p>
+        <Button id="btnBackHome" type="button" handleClick={clickBackHomeHandler} width="fit" variant="secondary">Volver al inicio</Button>
+      </div>
     </Main>
   )
 }
--- src/pages/error/NotFoundElement.tsx
+++ src/pages/error/NotFoundElement.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { Main } from "../../components/Main/Main";
-import { TitlePage } from "../../components/TitlePage/TitlePage";
 import { Button } from "../../components/Button/Button";
 
 interface NotFoundElementProps {
@@ -15,41 +14,56 @@ export function NotFoundElement({ elementType }: NotFoundElementProps) {
   if (elementType == "class") {
     return (
       <Main>
-        <TitlePage>Clase no encontrada</TitlePage>
-        <p className="text-lg sm:text-2xl text-center">La clase a la que intentas acceder no existe.</p>
-        <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver a la lista de clases</Button>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/4 xl:w-2/4 flex flex-col gap-3 sm:items-start items-center p-3">
+          <span className="text-9xl font-extrabold">404</span>
+          <h3 className="text-3xl">Clase no encontrada</h3>
+          <p className="text-lg sm:text-2xl w-full text-center sm:text-start">Lo sentimos, la clase que estás buscando no existe o ha sido eliminada. Verifica la dirección o regresa al listado para continuar navegando.</p>
+          <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver al listado de clases</Button>
+        </div>
       </Main>
     )
   } else if (elementType == "client") {
     return (
       <Main>
-        <TitlePage>Cliente no encontrado</TitlePage>
-        <p className="text-lg sm:text-2xl text-center">El cliente al que intentas acceder no existe.</p>
-        <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver a la lista de clientes</Button>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/4 xl:w-2/4 flex flex-col gap-3 sm:items-start items-center p-3">
+          <span className="text-9xl font-extrabold">404</span>
+          <h3 className="text-3xl">Cliente no encontrado</h3>
+          <p className="text-lg sm:text-2xl w-full text-center sm:text-start">Lo sentimos, el cliente que estás buscando no existe o ha sido eliminada. Verifica la dirección o regresa al listado para continuar navegando.</p>
+          <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver al listado de clientes</Button>
+        </div>
       </Main>
     )
   } else if (elementType == "employee") {
     return (
       <Main>
-        <TitlePage>Empleado no encontrado</TitlePage>
-        <p className="text-lg sm:text-2xl text-center">El empleado al que intentas acceder no existe.</p>
-        <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver a la lista de empleados</Button>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/4 xl:w-2/4 flex flex-col gap-3 sm:items-start items-center p-3">
+          <span className="text-9xl font-extrabold">404</span>
+          <h3 className="text-3xl">Empleado no encontrado</h3>
+          <p className="text-lg sm:text-2xl w-full text-center sm:text-start">Lo sentimos, el empleado que estás buscando no existe o ha sido eliminada. Verifica la dirección o regresa al listado para continuar navegando.</p>
+          <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver al listado de empleados</Button>
+        </div>
       </Main>
     )
   } else if (elementType == "offer") {
     return (
       <Main>
-        <TitlePage>Oferta no encontrada</TitlePage>
-        <p className="text-lg sm:text-2xl text-center">La oferta a la que intentas acceder no existe.</p>
-        <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver a la lista de oferta</Button>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/4 xl:w-2/4 flex flex-col gap-3 sm:items-start items-center p-3">
+          <span className="text-9xl font-extrabold">404</span>
+          <h3 className="text-3xl">Oferta no encontrado</h3>
+          <p className="text-lg sm:text-2xl w-full text-center sm:text-start">Lo sentimos, la oferta que estás buscando no existe o ha sido eliminada. Verifica la dirección o regresa al listado para continuar navegando.</p>
+          <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver al listado de ofertas</Button>
+        </div>
       </Main>
     )
   } else if (elementType == "report") {
     return (
       <Main>
-        <TitlePage>Incidencia no encontrada</TitlePage>
-        <p className="text-lg sm:text-2xl text-center">La incidencia a la que intentas acceder no existe.</p>
-        <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver a la lista de incidencia</Button>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/4 xl:w-2/4 flex flex-col gap-3 sm:items-start items-center p-3">
+          <span className="text-9xl font-extrabold">404</span>
+          <h3 className="text-3xl">Incidencia no encontrada</h3>
+          <p className="text-lg sm:text-2xl w-full text-center sm:text-start">Lo sentimos, la incidencia que estás buscando no existe o ha sido eliminada. Verifica la dirección o regresa al listado para continuar navegando.</p>
+          <Button id="btnBackHome" type="button" handleClick={clickBackListHandler} width="fit" variant="secondary">Volver al listado de incidencias</Button>
+        </div>
       </Main>
     )
   } else {
